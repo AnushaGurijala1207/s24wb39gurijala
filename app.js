11b7619c3ad7018723ca25f11d5afada2dcebb8d
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-03-25 16:07:11 -0500

    added pick and randomitem

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var instrumentRouter = require("./routes/instrument");
 var gridRouter = require("./routes/grid");
+var randomitemRouter = require("./routes/pick");
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/instrument", instrumentRouter);
 app.use("/grid", gridRouter);
+app.use("/randomitem", randomitemRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
